dbd9e45a835992697c0867dc3651df5684b928c3
neaguio <mihaicosminneagu@gmail.com>
2023-02-09 23:13:49 +0200

    feat: :sparkles: from, select, limit methods

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,59 @@
 import getCandles from './candles'
 import getCompanies from './companies'
 import getCurrentCandle from './current_candle'
-import { CurrentCandle, CurrentWeeklyCandles, InputGetCandles, Ticker } from './types'
+import { CurrentCandle, CurrentWeeklyCandles, InputGetCandles, Ticker, TIME_PERIOD } from './types'
 import getCurrentWeeklyCandles from './current_weekly_candles'
 import toSvg from './to_svg'
 
 class Trading212 {
   tickers: string[]
+  methodName : Function | null
+  data : any
+  options : any
+  size: number
+  periodType : TIME_PERIOD
+
   constructor(){
     this.tickers = [];
+    this.data = null;
+    this.methodName = null;
+    this.options = null;
+    this.size = 65;
+    this.periodType = 'ONE_DAY';
   }
-  select(tickers: Ticker){
+
+  from(tickers: Ticker){
     this.tickers = [tickers].flat();
     return this;
   }
-  companies(){
-    return getCompanies();
+  period(_periodType : TIME_PERIOD){
+    this.periodType = _periodType;
+    return this;
+  }
+  
+  limit(size :number){
+    if(typeof size !== "number"){
+      console.error('Invalid limit! Field must be number (default 65).')
+      return this;
+    }
+    this.size = size;
+    return size;
+  }
+  companies(){ 
+    this.methodName = getCompanies;
+    return this;
+  }
+  candles(){
+    this.methodName = getCandles
+    return this;
+  }
+  select(){
+    if(this.methodName) return this.methodName(this.options)
+    return null;
+  }
+  svg(){
+    // return toSvg(this.data);
+    return toSvg();
   }
   async getCandles(input: InputGetCandles[] | InputGetCandles) {
     return await getCandles(input)
